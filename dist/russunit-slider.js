fcbe6b95ad495a78e6bd6c0d60f1e58fa525fba8
Russell Chisholm <rchisholm@marian.org>
2020-03-23 01:16:14 -0400

    removed slideFadeIn, slideFadeReplace

--- dist/russunit-slider.js
+++ dist/russunit-slider.js
@@ -287,43 +287,6 @@ function Slider(options) {
 
   window.addEventListener('resize', this.resizeContainer);
   /**
-   * fades the first target out, then fades the second target in.
-   * @param {any} fadeOutTarget element to fade out, or its id
-   * @param {any} fadeInTarget element to fade in, or its id
-   * @param {function} callback function executed when fade is finished
-   * @param {{waitTime: any, display: any, fadeTime: number, toggleVisibility: boolean}} options options object for fade:
-   * options.waitTime: wait before executing - true for 2 sec, false for 0 sec, num for other (ms);
-   * options.display: display the target should have - true for flex, false for block, string for other;
-   * options.fadeTime: time for the fadeIn/fadeOut effects, defaults to 250;
-   * options.toggleVisibility: true if using visibility:hidden instead of display:none for fadeOut;
-   */
-
-  this.slideFadeReplace = function (fadeOutTarget, fadeInTarget) {
-    var callback = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : function () {};
-    var options = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : [];
-    // static values
-    var defaultWaitTime = 2000;
-    var defaultFadeTime = 250; // default options
-
-    options.waitTime = options.waitTime ? options.waitTime : false;
-    options.display = options.display ? options.display : false;
-    options.fadeTime = options.fadeTime ? options.fadeTime : defaultFadeTime;
-    options.toggleVisibility = options.toggleVisibility ? options.toggleVisibility : false;
-
-    if (options.waitTime) {
-      options.waitTime = options.waitTime === true ? defaultWaitTime : options.waitTime;
-      setTimeout(function () {
-        options.waitTime = false;
-
-        _this.slideFadeReplace(fadeOutTarget, fadeInTarget, callback, options);
-      }, options.waitTime);
-    } else {
-      _this.slideFadeOut(fadeOutTarget, function () {
-        _this.slideFadeIn(fadeInTarget, callback, options);
-      }, options);
-    }
-  };
-  /**
    * fades the target out
    * @param {element||string} fadeOutTarget element to fade out, or its id
    * @param {function} callback function executed when fade is finished
@@ -334,7 +297,6 @@ function Slider(options) {
    * options.direction: direction for the fading out element to fly away if position:aboslute (left, right, up, down) - null to stay still;
    * options.zoom: direction for the fading element to zoom if position:absolute (in, out) - null to stay same size
    */
-
 
   this.slideFadeOut = function (fadeOutTarget) {
     var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
@@ -488,88 +450,6 @@ function Slider(options) {
       }
     } else {
       console.log('fadeOut error: no such element exists.');
-    }
-  };
-  /**
-   * fades the target in
-   * @param {any} fadeInTarget element to fade in, or its id
-   * @param {function} callback function executed when fade is finished
-   * @param {{waitTime: any, display: any, fadeTime: number, toggleVisibility: boolean}} options options object for fade:
-   * options.waitTime: wait before executing - true for 2 sec, false for 0 sec, num for other (ms);
-   * options.display: display the target should have - true for flex, false for block, string for other;
-   * options.fadeTime: time for the fadeIn/fadeOut effects, defaults to 250;
-   * options.toggleVisibility: true if using visibility:hidden instead of display:none for fadeOut;
-   */
-
-
-  this.slideFadeIn = function (fadeInTarget) {
-    var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
-    var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
-
-    // check cb
-    if (typeof callback !== 'function') {
-      callback = function callback() {};
-    } // check target
-
-
-    if (typeof fadeInTarget === 'string') {
-      fadeInTarget = document.getElementById(fadeInTarget);
-    } // static values
-
-
-    var defaultWaitTime = 2000;
-    var defaultFadeTime = 250;
-    var intervalTime = 20; // default options
-
-    options.waitTime = options.waitTime ? options.waitTime : false;
-    options.display = options.display ? options.display : false;
-    options.fadeTime = options.fadeTime ? options.fadeTime : defaultFadeTime;
-    options.toggleVisibility = options.toggleVisibility ? options.toggleVisibility : false; // option values
-
-    options.display = options.display === false ? 'block' : options.display;
-    options.display = options.display === true ? 'flex' : options.display;
-    var isVisible = options.toggleVisibility ? function (element) {
-      return element.style.visibility !== "hidden";
-    } : function (element) {
-      return element.style.display !== "none";
-    };
-    var makeVisible = options.toggleVisibility ? function (element) {
-      element.style.visibility = "visible";
-    } : function (element) {
-      element.style.display = options.display;
-    };
-    options.fadeTime = typeof options.fadeTime === 'number' ? options.fadeTime : defaultFadeTime;
-
-    if (fadeInTarget) {
-      if (!isVisible(fadeInTarget)) {
-        if (options.waitTime) {
-          options.waitTime = options.waitTime === true ? defaultWaitTime : options.waitTime;
-          options.waitTime = typeof options.waitTime === 'number' ? options.waitTime : defaultWaitTime;
-          setTimeout(function () {
-            options.waitTime = false;
-
-            _this.slideFadeIn(fadeInTarget, callback, options);
-          }, options.waitTime);
-        } else {
-          if (fadeInTarget) {
-            var opacityInterval = intervalTime / options.fadeTime;
-            fadeInTarget.style.opacity = 0;
-            makeVisible(fadeInTarget);
-            var fadeInEffect = setInterval(function () {
-              if (fadeInTarget.style.opacity < 1) {
-                fadeInTarget.style.opacity = parseFloat(fadeInTarget.style.opacity) + parseFloat(opacityInterval);
-              } else {
-                clearInterval(fadeInEffect);
-                callback();
-              }
-            }, intervalTime);
-          }
-        }
-      } else {
-        callback(); // setTimeout(callback, options.fadeTime);
-      }
-    } else {
-      console.log('fadeIn error: no such element exists: ');
     }
   };
   /**
--- src/russunit-slider.js
+++ src/russunit-slider.js
@@ -263,43 +263,6 @@ class Slider {
 
         window.addEventListener('resize', this.resizeContainer);
 
-
-        /**
-         * fades the first target out, then fades the second target in.
-         * @param {any} fadeOutTarget element to fade out, or its id
-         * @param {any} fadeInTarget element to fade in, or its id
-         * @param {function} callback function executed when fade is finished
-         * @param {{waitTime: any, display: any, fadeTime: number, toggleVisibility: boolean}} options options object for fade:
-         * options.waitTime: wait before executing - true for 2 sec, false for 0 sec, num for other (ms);
-         * options.display: display the target should have - true for flex, false for block, string for other;
-         * options.fadeTime: time for the fadeIn/fadeOut effects, defaults to 250;
-         * options.toggleVisibility: true if using visibility:hidden instead of display:none for fadeOut;
-         */
-        this.slideFadeReplace = (fadeOutTarget, fadeInTarget, callback = () => {}, options = []) => {
-
-            // static values
-            const defaultWaitTime = 2000;
-            const defaultFadeTime = 250;
-
-            // default options
-            options.waitTime = options.waitTime ? options.waitTime : false;
-            options.display = options.display ? options.display : false;
-            options.fadeTime = options.fadeTime ? options.fadeTime : defaultFadeTime;
-            options.toggleVisibility = options.toggleVisibility ? options.toggleVisibility : false;
-
-            if (options.waitTime) {
-                options.waitTime = options.waitTime === true ? defaultWaitTime : options.waitTime;
-                setTimeout(() => {
-                    options.waitTime = false;
-                    this.slideFadeReplace(fadeOutTarget, fadeInTarget, callback, options);
-                }, options.waitTime);
-            } else {
-                this.slideFadeOut(fadeOutTarget, () => {
-                    this.slideFadeIn(fadeInTarget, callback, options);
-                }, options);
-            }
-        };
-
         /**
          * fades the target out
          * @param {element||string} fadeOutTarget element to fade out, or its id
@@ -447,88 +410,7 @@ class Slider {
             } else {
                 console.log('fadeOut error: no such element exists.');
             }
-        }
-
-        /**
-         * fades the target in
-         * @param {any} fadeInTarget element to fade in, or its id
-         * @param {function} callback function executed when fade is finished
-         * @param {{waitTime: any, display: any, fadeTime: number, toggleVisibility: boolean}} options options object for fade:
-         * options.waitTime: wait before executing - true for 2 sec, false for 0 sec, num for other (ms);
-         * options.display: display the target should have - true for flex, false for block, string for other;
-         * options.fadeTime: time for the fadeIn/fadeOut effects, defaults to 250;
-         * options.toggleVisibility: true if using visibility:hidden instead of display:none for fadeOut;
-         */
-        this.slideFadeIn = (fadeInTarget, callback = () => {}, options = []) => {
-
-            // check cb
-            if (typeof callback !== 'function') {
-                callback = () => {};
-            }
-
-            // check target
-            if (typeof fadeInTarget === 'string') {
-                fadeInTarget = document.getElementById(fadeInTarget);
-            }
-
-            // static values
-            const defaultWaitTime = 2000;
-            const defaultFadeTime = 250;
-            const intervalTime = 20;
-
-            // default options
-            options.waitTime = options.waitTime ? options.waitTime : false;
-            options.display = options.display ? options.display : false;
-            options.fadeTime = options.fadeTime ? options.fadeTime : defaultFadeTime;
-            options.toggleVisibility = options.toggleVisibility ? options.toggleVisibility : false;
-
-            // option values
-            options.display = options.display === false ? 'block' : options.display;
-            options.display = options.display === true ? 'flex' : options.display;
-            var isVisible = options.toggleVisibility ? (element) => {
-                return element.style.visibility !== "hidden";
-            } : (element) => {
-                return element.style.display !== "none";
-            };
-            var makeVisible = options.toggleVisibility ? (element) => {
-                element.style.visibility = "visible";
-            } : (element) => {
-                element.style.display = options.display;
-            };
-            options.fadeTime = typeof options.fadeTime === 'number' ? options.fadeTime : defaultFadeTime;
-
-            if (fadeInTarget) {
-                if (!isVisible(fadeInTarget)) {
-                    if (options.waitTime) {
-                        options.waitTime = options.waitTime === true ? defaultWaitTime : options.waitTime;
-                        options.waitTime = typeof options.waitTime === 'number' ? options.waitTime : defaultWaitTime;
-                        setTimeout(() => {
-                            options.waitTime = false;
-                            this.slideFadeIn(fadeInTarget, callback, options);
-                        }, options.waitTime);
-                    } else {
-                        if (fadeInTarget) {
-                            var opacityInterval = intervalTime / options.fadeTime;
-                            fadeInTarget.style.opacity = 0;
-                            makeVisible(fadeInTarget);
-                            var fadeInEffect = setInterval(() => {
-                                if (fadeInTarget.style.opacity < 1) {
-                                    fadeInTarget.style.opacity = parseFloat(fadeInTarget.style.opacity) + parseFloat(opacityInterval);
-                                } else {
-                                    clearInterval(fadeInEffect);
-                                    callback();
-                                }
-                            }, intervalTime);
-                        }
-                    }
-                } else {
-                    callback();
-                    // setTimeout(callback, options.fadeTime);
-                }
-            } else {
-                console.log('fadeIn error: no such element exists: ');
-            }
-        }
+        };
 
         /**
          * get the index of the next slide
